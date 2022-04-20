00ae300462bdd3f784b4447ccb140d1f8bc23133
Rudy Orre <44660069+rudyorre@users.noreply.github.com>
2022-04-20 13:40:21 -0700

    Lab 2 source code
    - TA verified the output

--- lab2/fpconverter.v
+++ lab2/fpconverter.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
+//
 // Company: 
 // Engineer: 
 // 
@@ -17,7 +17,7 @@
 // Revision 0.01 - File Created
 // Additional Comments: 
 //
-//////////////////////////////////////////////////////////////////////////////////
+//
 module fpconverter(d, S, E, F);
 
 // Input and output declaration
@@ -28,7 +28,9 @@ output wire [3:0] F;
 
 reg [11:0] d_manip; //for two's complement if negative
 reg [2:0] zero_count; //to count leading zeros
+reg [2:0] zero_count_updated;
 reg [3:0] significand;
+reg [3:0] significand_added;
 reg round_up_Flag;
 
 //Assign the sign bit
@@ -37,28 +39,83 @@ assign S = d[11];
 //If the sign is negative, we need to flip
 always @* begin
 	if (S == 1) begin
-		assign d_manip = ~d + 1'b1;
+		//Edge case for most negative number
+		if (d == 12'b100000000000) begin 
+			assign d_manip = ~d; end
+		//Normal case
+		else assign d_manip = ~d + 1'b1;
 	end
+
 	else begin
-		assign d_manip = d;
+		assign d_manip = d; 
 	end
 end
+
+
 
 //Counting leading zeros using priority encoder
+//Getting the Significand and the Rounding bit
 always @* begin
-	if (d_manip[10] == 1) assign zero_count = 7;
-	else if (d_manip[9] == 1) assign zero_count = 6;
-	else if (d_manip[8] == 1) assign zero_count = 5; 
-	else if (d_manip[7] == 1) assign zero_count = 4; 
-	else if (d_manip[6] == 1) assign zero_count = 3; 
-	else if (d_manip[5] == 1) assign zero_count = 2; 
-	else if (d_manip[4] == 1) assign zero_count = 1; 
-	else assign zero_count = 0; 	
+	if (d_manip[10] == 1) begin 
+		assign zero_count = 7;
+		assign significand = d_manip[10:7]; 
+		assign round_up_Flag = d_manip[6]; end
+	
+	else if (d_manip[9] == 1) begin
+		assign zero_count = 6; 
+		assign significand = d_manip[9:6]; 
+		assign round_up_Flag = d_manip[5]; end
+		
+	else if (d_manip[8] == 1) begin 
+		assign zero_count = 5;
+		assign significand = d_manip[8:5]; 
+		assign round_up_Flag = d_manip[4]; end
+	
+	else if (d_manip[7] == 1) begin
+		assign zero_count = 4;
+		assign significand = d_manip[7:4]; 
+		assign round_up_Flag = d_manip[3]; end	
+		
+	else if (d_manip[6] == 1) begin
+		assign zero_count = 3; 
+		assign significand = d_manip[6:3]; 
+		assign round_up_Flag = d_manip[2]; end
+		
+	else if (d_manip[5] == 1) begin 
+		assign zero_count = 2;
+		assign significand = d_manip[5:2]; 
+		assign round_up_Flag = d_manip[1]; end
+		
+	else if (d_manip[4] == 1) begin 
+		assign zero_count = 1;
+		assign significand = d_manip[4:1]; 
+		assign round_up_Flag = d_manip[0]; end	
+		
+	else begin 
+		assign zero_count = 0; 	
+		assign significand = d_manip[3:0]; 
+		assign round_up_Flag = 0; end
 end
 
-//Getting the Significand
-assign significand = d_manip << zero_count;
+//Rounding
+always @* begin
+	if (significand == 4'b1111 && round_up_Flag) begin 
+		if (zero_count == 7) begin 
+			assign significand_added = significand;
+			assign zero_count_updated = zero_count; 
+		end
+		else begin
+			assign significand_added = significand >> 1;
+			assign zero_count_updated = zero_count + 1; 
+		end
+	end
+	
+	else begin 
+		assign significand_added = significand + (1 & round_up_Flag); 
+		assign zero_count_updated = zero_count; end
+end
 
-assign E = zero_count;
+assign E = zero_count_updated;
+assign F = significand_added;
 
 endmodule
--- lab2/fpconverterTB.v
+++ lab2/fpconverterTB.v
@@ -36,16 +36,62 @@ module fpconverterTB;
 	);
 
 	initial begin
-		// Initialize Inputs
-		d = 12'b001000000000;
-
-		// Wait 100 ns for global reset to finish
+		//Initialization
+		d = 0;
 		#1000;
 		
-		//Add stimulus here
-		d = 12'b101000000000;
-        
+		$display("Begin Test Cases -----------");
 
+		//Most Negative Number
+		d = 12'b100000000000;
+		#10
+		$display("\nInitial -- Most Neg: %b \nFloating Point: %b %b %b", d, S, E, F);
+		
+		#1000
+		//Most Positive Number
+		d = 12'b011111111111;
+		#10
+		$display("\nInitial -- Most Pos: %b \nFloating Point: %b %b %b", d, S, E, F);
+		
+		#1000;
+		
+		//Rounding #1
+		d = 12'b000000101100;
+		#10
+		$display("\nInitial -- Rounding #1: %b \nFloating Point: %b %b %b", d, S, E, F);
+		
+		#1000
+		//Rounding #2
+		d = 12'b000000101101;
+		#10
+		$display("\nInitial -- Rounding #2: %b \nFloating Point: %b %b %b", d, S, E, F);
+		
+		#1000
+		//Rounding #3
+		d = 12'b000000101110;
+		#10
+		$display("\nInitial -- Rounding #3: %b \nFloating Point: %b %b %b", d, S, E, F);
+		
+		#1000
+		//Rounding #4
+		d = 12'b000000101111;
+		#10
+		$display("\nInitial -- Rounding #4: %b \nFloating Point: %b %b %b", d, S, E, F);
+		
+		#1000
+		//Extra #1
+		d = 12'b011111000000;
+		#10
+		$display("\nInitial -- Extra #1: %b \nFloating Point: %b %b %b", d, S, E, F);
+
+
+		
+		#1000;
+
+		//Add stimulus here
+		d = 0;
+        
+		$display("\n---------- End Test Cases");
 	end
       
 endmodule
